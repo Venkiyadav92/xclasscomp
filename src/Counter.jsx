21046bb4ac166eb0ab6d09740fbabbdfbafa3470
Venkatesh S <vyvenkateshyadav@gmail.com>
2024-06-11 14:15:56 +0530

    Counter application

---
+++ src/Counter.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    const counterStyle = {
+      marginLeft: '10px',
+    };
+
+    return (
+      <div style={counterStyle}> 
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
